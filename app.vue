08a59f25df0ce65f27e18b84b71abec6cc539caf
Parsons <parsons@ParsonsdeMacBook-Air.local>
2022-10-17 18:27:16 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="m-4 bg-white">
+        <Icon name="logos:nuxt" size="360" />
+        <p class="pb-4 text-2xl text-slate-600">這裡是最外層 app.vue</p>
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+        <IronManWelcome />
+    </div>
+</template>
+
+<script lang="ts" setup>
+// const year: number = 1
+// const title: string = `${year} iThome 鐵人賽`
+</script>
