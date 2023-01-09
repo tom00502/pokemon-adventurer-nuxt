d04f94104a700f1a9ca0faa8e7cc5fa7f5d1e143
Parsons <gongyingparsons@gmail.com>
2023-01-09 10:11:36 +0800

    親密查表加廣告

--- pages/candyValue.vue
+++ pages/candyValue.vue
@@ -8,10 +8,27 @@ const showTable = computed(() => {
     if (!searchText.value) return candyValues
     return candyValues.filter((value) => value.stat === searchText.value)
 })
+onMounted(async () => {
+    setTimeout(() => {
+        const childList = document.getElementsByClassName('focusAd')
+        // console.log('length', childList.length)
+        for (let i = 0; i < childList.length; i++) {
+            ;(adsbygoogle = window.adsbygoogle || []).push({})
+        }
+    }, 500)
+})
 </script>
 
 <template>
     <main>
+        <ins
+            class="adsbygoogle focusAd"
+            style="display: block"
+            data-ad-client="ca-pub-2683150416576260"
+            data-ad-slot="6422833388"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+        ></ins>
         <div class="page-title">親密度提升表</div>
         <div class="note">
             <ul>
